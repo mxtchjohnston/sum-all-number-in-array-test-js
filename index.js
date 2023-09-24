47d94105febf59bb4686863fae071579d5c60def
Mitchell Stave <mitchjohnston99@gmail.com>
2023-09-24 15:30:28 -0700

    4/4 tests passing

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for (let elem of array) {
+    if (Array.isArray(elem)) {
+      sum += sumItems(elem);
+      continue;
+    }
+
+    sum += elem;
+  }
+
+  return sum;
 }
 
 module.exports = sumItems;
